0f8ae5de9a927ad8bbb2a0df1e3489f8197f16de
zhongyah <18615535108@163.com>
2018-03-20 21:21:16 +0800

    爬取卫生计生通知信息
    添加正则表达式匹配和邮件发送流程，爬取信息

--- whwjw.py
+++ whwjw.py
@@ -10,71 +10,68 @@ import html
 
 
 url = 'http://www.whws.gov.cn/col/col29396/index.html'
+mailto_list=['touser@qq.com']           #收件人(列表)
+mail_host="smtp.163.com"            #使用的邮箱的smtp服务器地址，这里是163的smtp地址
+mail_user="fromuser@163.com"                           #用户名
+mail_pass="password"                             #密码
+mail_postfix="163.com"
 
 #获得网页源码
-data = requests.get(url)
-data.encoding='utf-8' #显式地指定网页编码，一般情况可以不用
-# print(data.text)
-#图片正则表达式
-regex = r'(CDATA[)(.*]]&)'
-
-#re是一个列表
-# pa = re.compile(regex) #创建一个pa模板，使其符合匹配的网址
-wzSoup =bs4.BeautifulSoup(data.text,"html.parser")
-wzIndex = wzSoup.select('#List1')
-# print(wzIndex[0])
-s=str(wzIndex[0].text)
-s = s.replace("&lt;", "<")
-print(s)
-str =html.escape(wzIndex[0].text,'')
-Elems =bs4.BeautifulSoup(wzIndex[0].text,"html.parser")
-webSite =Elems.select('title')
+def getdata(url):
+    data = requests.get(url)
+    data.encoding='utf-8' #显式地指定网页编码，一般情况可以不用
 
 
+    #re是一个列表
+    # pa = re.compile(regex) #创建一个pa模板，使其符合匹配的网址
+    wzSoup =bs4.BeautifulSoup(data.text,"html.parser")
+    wzIndex = wzSoup.select('#List1')
+    # print(wzIndex[0])
+    s=str(wzIndex[0].text)
+    s = s.replace("&lt;", "<")
+    # print(s)
+    # str =html.escape(wzIndex[0].text,'')
+    # Elems =bs4.BeautifulSoup(wzIndex[0].text,"html.parser")
+    # webSite =Elems.select('title')
+    #图片正则表达式
+    pattern_title = r"bt_link' title='(.*?)' target="
+    pattern_href = r"<a  style=\"font-size:14px;\" href='(.*?)'"
+    pattern_date = r'class=\"bt_time\" style=\"font-size:14px;border-bottom:dashed 1px #ccc">\[(.*?)\]'
+    # pa = re.compile(regex)
+    title = re.findall(pattern_title,s)
+    href = re.findall(pattern_href,s)# return a list
+    date = re.findall(pattern_date,s)
+
+    for (str_title ,str_href,str_date) in zip(title,href,date):
+        # print(str_date+'   '+ str_title +' \n  '+'http://www.whws.gov.cn'+str_href)
+        for i in range(1):  # 发送1封，上面的列表是几个人，这个就填几
+            if send_mail(mailto_list,  str_title, str_date+'   '+ str_title +'  \n '+'http://www.whws.gov.cn'+str_href):  # 邮件主题和邮件内容\n change row  \r enter
+                # 这是最好写点中文，如果随便写，可能会被网易当做垃圾邮件退信
+                print(str_title+str_date+"邮件发送成功!")
+            else:
+                print("发送失败!")
+# print(len(title))# return list elements count
+# print(len(href))
+# print(len(date))
 
 
-my_sender='xiao..ao@163.com'    # 发件人邮箱账号
-my_pass = '55girl'              # 发件人邮箱密码(当时申请smtp给的口令)
-my_user='xiao..ao@163.com'      # 收件人邮箱账号，我这边发送给自己
-def mail():
-    ret=True
+
+                  #邮箱的后缀，网易就是163.com
+def send_mail(to_list,sub,content):
+    me="hello"+"<"+mail_user+"@"+mail_postfix+">"
+    msg = MIMEText(content,_subtype='plain')
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)                #将收件人列表以‘；’分隔
     try:
-        msg=MIMEText('填写邮件内容','plain','utf-8')
-        msg['From']=formataddr(["发件人昵称",my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-        msg['To']=formataddr(["收件人昵称",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-        msg['Subject']="邮件主题-测试"                # 邮件的主题，也可以说是标题
-
-        server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465
-        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
-        server.sendmail(my_sender,[my_user,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
-        server.quit()# 关闭连接
-    except Exception:# 如果 try 中的语句没有执行，则会执行下面的 ret=False
-        ret=False
-    return ret
-
-ret=mail()
-if ret:
-    print("邮件发送成功")
-else:
-    print("邮件发送失败")
-
-# print(webSite[0].text)
-# ma = re.findall(regex,data.text) #findall 方法找到data中所有的符合pa的对象，添加到re中并返回
-# print(ma)
-#图片的名字
-# i=0
-
-#要在控制台打印提示，了解进程
-# print('Start downloading')
-
-#将ma中图片网址依次提取出来
-# for image in ma:
-#     i+=1
-#     image = requests.get(image).content
-#     print(str(i)+'.jpg is downloading')
-#
-#     # \ 要用转义符号 \\表示，要注意原图片的格式
-#     with open('D:\Python Study\crawl\crawl pictures\\'+str(i)+'.jpg','wb') as f: #注意打开的是就jpg文件
-#          f.write(image)
-
-# print('Finish downloading')
+        server = smtplib.SMTP()
+        server.connect(mail_host)                            #连接服务器
+        server.login(mail_user,mail_pass)               #登录操作
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
+        return True
+    except Exception:
+        # print str(e)
+        return False
+if __name__ == '__main__':
+    pt = getdata(url)
